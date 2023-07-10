bd984c0a24a260c167890b37e042b7d2e14e7dbc
Viraj Bukitagar <viraj.bukitagar@appdynamics.com>
2023-07-10 16:03:24 +0530

    Adding the PropertiesLogger to debug prod issue.

--- src/main/java/com/polarbookshop/dispatcherservice/DispatcherServiceApplication.java
+++ src/main/java/com/polarbookshop/dispatcherservice/DispatcherServiceApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DispatcherServiceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DispatcherServiceApplication.class, args);
+        SpringApplication springApplication = new SpringApplication(DispatcherServiceApplication.class);
+        springApplication.addListeners(new PropertiesLogger());
+        springApplication.run(args);
     }
 
 }
